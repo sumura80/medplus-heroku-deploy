ec0504b589778a3e56c4de314f1acb7e2031f1bb
Susumu <sumura80@gmail.com>
2019-02-19 22:08:15 +0900

    add users super

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,14 @@ class User < ApplicationRecord
   mount_uploader :user_image, ImageUploader
 
   #usernameを確実に入力させるためのvalidates usernameがuniqueであることと、
-  #文字数が4から20文字であることを設定出来ます。
+  #文字数が3から20文字であることを設定しています。
   #(case_sensitive: :falssは大文字小文字の区別をしないということ）
   validates :username,
    uniqueness: { case_sensitive: :true },
   length: { minimum: 3, maximum: 20 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   
  #↓論理削除をするためのparanoiaの導入
   acts_as_paranoid
