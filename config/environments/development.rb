70107be9f2ff3f7231d346f50556c575b634eb82
Susumu <sumura80@gmail.com>
2019-04-13 11:22:28 +0900

    commit log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,19 +54,19 @@ Rails.application.configure do
 
   #このサイトを参考　　http://c5meru.hatenablog.jp/entry/2018/07/29/200011
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.delivery_method = :smtp
   #ActionMailer::Base.delivery_method = :letter_opener
-  #config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web
 
-  ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  domain: 'gmail.com',
-  port: 587,
-  user_name: ENV['ACTION_MAILER_EMAIL_ADDRESS'],
-  password: ENV['ACTION_MAILER_EMAIL_PASSWORD'],
+#   ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.gmail.com',
+#   domain: 'gmail.com',
+#   port: 587,
+#   user_name: ENV['ACTION_MAILER_EMAIL_ADDRESS'],
+#   password: ENV['ACTION_MAILER_EMAIL_PASSWORD'],
 
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
+#   authentication: 'plain',
+#   enable_starttls_auto: true
+# }
 
 end
